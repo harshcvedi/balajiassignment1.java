4d155d3fe8296da7eddd57aef113852a31496cc1
harshcvedi <105970898+harshcvedi@users.noreply.github.com>
2022-12-12 12:47:56 +0530

    Create pascals_triangle.java

---
+++ pascals_triangle.java
@@ -0,0 +1,16 @@
+public static void main(String args[]){
+
+   Scanner sc=new Scanner(System.in);
+   int n=sc.nextInt();
+   int k=1;
+   for(int i=0;i<n;i++){
+      k=1;
+      for(int j=0;j<=i;j++){
+         System.out.print(k+" ");
+            k=((i-j)*k)/(j+1);
+            
+
+      }System.out.println();
+   }
+}//pascals triangle
+}
