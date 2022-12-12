0ce0ffc9ee0850f5e514e0fd400163921780eb2f
harshcvedi <105970898+harshcvedi@users.noreply.github.com>
2022-12-12 12:15:47 +0530

    Create mirror_star_pattern.java

---
+++ mirror_star_pattern.java
@@ -0,0 +1,22 @@
+    public static void main(String args[]) {
+             Scanner sc =new Scanner(System.in);
+        int N =sc.nextInt();
+        for(int i=1;i<=(N/2)+1;i++){
+         for(int j=1;j<=(N/2)+1-i;j++){
+            System.out.print("   ");
+         } for(int j=1;j<=2*i-1;j++){
+            System.out.print("*"+"  ");
+         }System.out.println();
+      }
+
+      for(int i=(N/2);i>=1;i--){
+         for(int j=1;j<=(N/2)+1-i;j++){
+            System.out.print("   ");
+         } for(int j=1;j<=2*i-1;j++){
+            System.out.print("*"+"  ");
+         }System.out.println();
+      }
+   }
+}
+   
+   
