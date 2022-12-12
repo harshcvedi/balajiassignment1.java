be8ef0e7d39bf7d219e3374cd1233dd56760e3d4
harshcvedi <105970898+harshcvedi@users.noreply.github.com>
2022-12-12 12:37:50 +0530

    Create simple_input.java

---
+++ simple_input.java
@@ -0,0 +1,23 @@
+public static void main(String args[]) {
+                Scanner scn = new Scanner(System.in);
+        int n;
+        int sum = 0;
+
+        while(scn.hasNextInt())
+        {
+            n = scn.nextInt();
+            sum = sum + n;
+            if(n > -1000 && n < 1000)
+            {
+                if(sum >= 0)
+                {
+                    System.out.println(n);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
