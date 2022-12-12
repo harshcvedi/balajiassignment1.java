b73be5060791f36046ed42cb1a364af69e0a061a
harshcvedi <105970898+harshcvedi@users.noreply.github.com>
2022-12-12 12:25:53 +0530

    Create print_series.java

---
+++ print_series.java
@@ -0,0 +1,20 @@
+    public static void main(String args[]) {
+Scanner s = new Scanner(System.in);
+		int N1 = s.nextInt();
+		int N2 = s.nextInt();
+
+		
+
+		for(int n = 1; N1 >0; n++)	
+		{
+			int value = 3*n+2;
+			if (value % N2 != 0)
+			{
+				System.out.println(value);
+				N1--;
+			}
+		}
+	}
+}
+        
+       
